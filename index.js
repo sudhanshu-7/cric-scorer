5d1c8b3ba63750471defd25ec84c92cb4c882cdc
sudhanshu-7 <sudhanshunautiyal777@gmail.com>
2021-05-15 19:18:39 +0530

    Process Port Change

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-const apiPort= 8000
+const apiPort= process.env.PORT || 8000
 
 app.get("/",(req,res)=>{
     res.send("Hello")

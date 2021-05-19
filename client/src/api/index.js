465352ef09d1a09a05181700fd3db97d3eb9d738
sudhanshu-7 <66016932+sudhanshu-7@users.noreply.github.com>
2021-05-19 21:15:21 +0530

    change the api link

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL:"http://localhost:8000/"
+    baseURL:"https://crickscorer.herokuapp.com/"
 })
 
 export const insertScore = payload => api.post("/score",payload)
